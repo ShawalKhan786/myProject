0f8efdf2a6c293cce8fe37b58dab654d08437a3d
ShawalKhan786 <shawalkhnz@gmail.com>
2023-09-13 18:11:11 +0200

    added home page content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,76 @@
 <template>
 
-    <div class="about"><v-card height="1000px"><h1 class="mt-15">It will be Updated Soon...</h1></v-card>
-      
-    </div>
+   <v-container>
+<v-card class="mt-15">
+
+</v-card>
+
+      <v-row>
+        <v-col cols="12 col-md-6 mt-12">
+          <h2 style="font-style: oblique;">
+       I help students for Study in Germany as well as I guide students in career
+            <br>Now you are here on the right place & in right hands
+          </h2>
+        </v-col>
+        <v-col cols="12 col-md-6 mt-12"> <img src="studyandcareer.png" alt="" style="max-height: 70%; max-width:80%"  ></v-col>
+        <v-col cols="12 text-center"> <!-- Add a v-col for centering -->
+    <h2>"Every path is difficult until you take the first step"</h2>
+  </v-col>
+      </v-row>
+
+      <v-row style="background-color: #f7f7f7">
+        <v-col cols="12 col-md-12 mt-9 " >
+<h1 class="mb-9">Welcome to Our Study and Career Guidance </h1>
+<p>
+  I'm here to help you navigate the exciting journey of studying and building a career in Germany. My mission is to provide you with valuable insights, guidance, and support to make informed decisions about your educational and career aspirations.
+</p>
+<p>
+  Whether you're a student considering higher education options, a professional seeking career opportunities, or someone exploring the possibilities in Germany, I'm here to assist you every step of the way.
+</p>
+        </v-col>
+        <v-col cols="12 col-md-6" ></v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12"> <h1>Services Include:</h1></v-col>
+        <v-col cols="6 col-md-6" >
+          <h2>Study in Germany:</h2>
+          <p>
+            I can guide you through the process of applying to German universities, understanding admission requirements, and choosing the right program for your goals.
+          </p>
+        </v-col>
+        <v-col cols="6 col-md-6" >
+          <h2>Career Development: </h2>
+          <p>
+            I offer insights into career prospects in Germany, including job market trends, networking tips, and advice on building a successful career.
+          </p>
+        </v-col>
+        <v-col cols="6 col-md-6" >
+          <h2>Visa and Immigration:</h2>
+          <p>
+            My expertise extends to visa and immigration processes, helping you understand the necessary steps to legally reside and work in Germany.
+          </p>
+        </v-col>
+        <v-col cols="6 col-md-6" >
+          <h2>General Guidance:</h2>
+          <p>
+            Have questions about life in Germany, cultural adaptation, or any other aspect of your journey? Feel free to reach out; I'm here to assist you.  
+          </p>
+        </v-col>
+      </v-row>
+      <v-row style="background-color: #f7f7f7">
+        <v-col cols="12">
+          <h1>
+            Connect with Me:
+          </h1>
+          <p>
+            I'm just a message away! If you have questions, need information, or want to discuss your study and career plans in Germany, don't hesitate to contact me. I'm dedicated to providing you with accurate and up-to-date information to help you make the most of your opportunities in Germany.
+
+Your dreams of studying and thriving in Germany are within reach, and I'm here to make your journey as smooth as possible. Let's embark on this exciting adventure together!
+          </p>
+        </v-col>
+      </v-row>
+   
+   </v-container>
   </template>
 
 <script>
